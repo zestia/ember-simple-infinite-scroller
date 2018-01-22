fb80c13bbfc717778d9d0916071439f1a5c5a277
Andrew Kirwin <andrew.kirwin@zestia.com>
2018-01-22 09:56:49 +0000

    upgrade ember cli and run ember init

--- testem.js
+++ testem.js
@@ -1,7 +1,3 @@
-/* eslint-disable camelcase */
-
-'use strict';
-
 module.exports = {
   test_page: 'tests/index.html?hidepassed',
   disable_watching: true,
@@ -15,11 +11,14 @@ module.exports = {
     Chrome: {
       mode: 'ci',
       args: [
+        // --no-sandbox is needed when running Chrome inside a container
+        process.env.TRAVIS ? '--no-sandbox' : null,
+
         '--disable-gpu',
         '--headless',
         '--remote-debugging-port=0',
         '--window-size=1440,900'
-      ]
+      ].filter(Boolean)
     }
   }
 };
