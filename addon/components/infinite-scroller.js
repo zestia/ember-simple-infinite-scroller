0a6f64969b32049dd38b6f8c758585b4421face6
Andrew Kirwin <andrew.kirwin@zestia.com>
2018-08-23 17:20:33 +0100

    being work in progress refactor to fix loadMore bug

--- addon/components/infinite-scroller.js
+++ addon/components/infinite-scroller.js
@@ -3,7 +3,7 @@ import layout from '../templates/components/infinite-scroller';
 import { bind, debounce, cancel } from '@ember/runloop';
 import { resolve } from 'rsvp';
 import { inject } from '@ember/service';
-import { trySet } from '@ember/object';
+import { getWithDefault, trySet } from '@ember/object';
 const { round } = Math;
 
 export default Component.extend({
@@ -13,18 +13,16 @@ export default Component.extend({
   classNames: ['infinite-scroller'],
   classNameBindings: ['isLoading'],
 
-  debug: false,
+  debug: true,
 
   didInsertElement() {
     this._super(...arguments);
-    this.set('_scrollHandler', bind(this, '_scroll'));
-    this._listener().addEventListener('scroll', this.get('_scrollHandler'));
+    this._listen();
   },
 
   willDestroyElement() {
     this._super(...arguments);
-    this._listener().removeEventListener('scroll', this.get('_scrollHandler'));
-    cancel(this.get('_scrollDebounceId'));
+    this._stopListening();
   },
 
   actions: {
@@ -33,84 +31,112 @@ export default Component.extend({
     }
   },
 
+  _listen() {
+    this._scrollHandler = bind(this, '_scroll');
+    this._listener().addEventListener('scroll', this._scrollHandler);
+  },
+
+  _stopListening() {
+    this._listener().removeEventListener('scroll', this._scrollHandler);
+    cancel(this._scrollDebounceId);
+  },
+
   _scroll(e) {
-    this.set('_scrollDebounceId', debounce(this, '_debouncedScroll', e, this._scrollDebounce()));
+    this._scrollDebounceId = debounce(this, '_debouncedScroll', e, this._scrollDebounce());
   },
 
   _debouncedScroll() {
-    if (this.get('debug')) {
-      this._debug();
-    }
-
     if (this._shouldLoadMore()) {
       this._loadMore();
     }
   },
 
+  _scrollDebounce() {
+    return getWithDefault(this, 'scrollDebounce', 100);
+  },
+
+  _bottom() {
+    return parseInt(getWithDefault(this, 'bottom', '100%'), 10);
+  },
+
   _listener() {
-    if (this.get('use-document')) {
-      return this.get('_infiniteScroller.document');
-    } else if (this.get('use-element')) {
-      return this.get('element').querySelector(this.get('use-element'));
+    if (this.useDocument) {
+      return this._infiniteScroller.document;
     } else {
-      return this.get('element');
+      return this._element();
     }
   },
 
   _element() {
-    if (this.get('use-document')) {
-      return this.get('_infiniteScroller.documentElement');
-    } else if (this.get('use-element')) {
-      return this.get('element').querySelector(this.get('use-element'));
+    if (this.selector) {
+      return this.element.querySelector(this.selector);
     } else {
-      return this.get('element');
+      return this.element;
     }
-  },
-
-  _scrollDebounce() {
-    return this.get('scroll-debounce') || 100;
-  },
-
-  _scrollerHeight() {
-    return this._element().clientHeight;
-  },
-
-  _scrollableHeight() {
-    return this._element().scrollHeight;
-  },
-
-  _scrollTop() {
-    if (this.get('use-document')) {
-      return this.get('_infiniteScroller.window.pageYOffset');
-    } else {
-      return this._element().scrollTop;
-    }
-  },
-
-  _scrollerBottom() {
-    return this._scrollableHeight() - this._scrollerHeight();
-  },
-
-  _scrollPercentage() {
-    return round(this._scrollTop() / this._scrollerBottom() * 100);
-  },
-
-  _triggerAt() {
-    return parseInt(this.get('trigger-at') || '100%', 10);
-  },
-
-  _reachedBottom() {
-    return this._scrollPercentage() >= this._triggerAt();
   },
 
   _shouldLoadMore() {
-    return this._reachedBottom() && !this.get('isLoading');
+    if (this.useDocument) {
+      return this._bottomOfElementHasBecomeVisbleInDocument();
+    } else {
+      return this._bottomOfElementHasBecomeVisible();
+    }
+  },
+
+  _bottomOfElementHasBecomeVisbleInDocument() {
+    const clientHeight = this._infiniteScroller.document.documentElement.clientHeight;
+    const bottom = this._element().getBoundingClientRect().bottom;
+    const pixelsToBottom = bottom - clientHeight;
+    const percentageToBottom = ((bottom - pixelsToBottom) / bottom) * 100;
+    const reachedBottom = percentageToBottom >= this._bottom();
+
+    if (this.debug) {
+      /* eslint-disable no-console */
+      console.table([
+        {
+          clientHeight,
+          bottom,
+          pixelsToBottom,
+          percentageToBottom,
+          reachedBottom
+        }
+      ]);
+    }
+
+    return reachedBottom;
+  },
+
+  _bottomOfElementHasBecomeVisible() {
+    const scrollHeight = this._element().scrollHeight;
+    const scrollTop = this._element().scrollTop;
+    const clientHeight = this._element().clientHeight;
+    const bottom = scrollHeight - clientHeight;
+    const pixelsToBottom = bottom - scrollTop;
+    const percentageToBottom = ((bottom - pixelsToBottom) / bottom) * 100;
+    const reachedBottom = percentageToBottom >= this._bottom();
+
+    if (this.debug) {
+      /* eslint-disable no-console */
+      console.table([
+        {
+          scrollHeight,
+          scrollTop,
+          clientHeight,
+          bottom,
+          pixelsToBottom,
+          percentageToBottom,
+          reachedBottom
+        }
+      ]);
+    }
+
+    return reachedBottom;
   },
 
   _loadMore() {
     this.set('error', null);
     this.set('isLoading', true);
-    resolve(this.get('on-load-more')())
+    resolve(this.onLoadMore())
       .catch(bind(this, '_loadError'))
       .finally(bind(this, '_loadFinished'));
   },
@@ -121,20 +147,5 @@ export default Component.extend({
 
   _loadFinished() {
     trySet(this, 'isLoading', false);
-  },
-
-  _debug() {
-    /* eslint-disable no-console */
-    console.table([{
-      'scroller height': this._scrollerHeight(),
-      'scrollable height': this._scrollableHeight(),
-      'scroll top': this._scrollTop(),
-      'scroller bottom': this._scrollerBottom(),
-      'scroll percentage': this._scrollPercentage(),
-      'reached bottom': this._reachedBottom(),
-      'should load more': this._shouldLoadMore(),
-      'scroll debounce': this._scrollDebounce(),
-      'trigger at': this._triggerAt()
-    }]);
-  },
+  }
 });
