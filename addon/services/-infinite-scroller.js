c330eecd65e8002f7633cb9d39326dc0e2fabef6
Maros Hluska <mhluska@gmail.com>
2018-10-04 14:02:26 +0800

    Remove whitespace

--- addon/services/-infinite-scroller.js
+++ addon/services/-infinite-scroller.js
@@ -5,7 +5,7 @@ const isFastBoot = typeof FastBoot !== 'undefined';
 
 export default Service.extend({
   debug: false,
-  
+
   document: computed(function() {
     return isFastBoot ? null : document;
   }),
@@ -14,12 +14,12 @@ export default Service.extend({
     const doc = this.get('document');
     return doc ? doc.documentElement : null;
   }),
-  
+
   init() {
     this._super(...arguments);
     this.set('_log', []);
   },
-  
+
   log(state) {
     /* eslint-disable no-console */
     this._log.push(state);
