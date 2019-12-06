de4dff1960e75ab06a94d61b91a7ac44db804e1e
Andrew Kirwin <andrew.kirwin@gmail.com>
2019-12-06 20:29:36 +0000

    upgrade deps

--- tests/dummy/app/_controllers/things.js
+++ tests/dummy/app/_controllers/things.js
@@ -1,33 +1,33 @@
 import { later } from '@ember/runloop';
 import { Promise } from 'rsvp';
+import { action, set, get, getWithDefault } from '@ember/object';
 import Controller, { inject as injectController } from '@ember/controller';
 import generateThings from '../utils/generate-things';
 
-export default Controller.extend({
+export default class ThingsController extends Controller {
+  @injectController('application') appController;
+
   init() {
-    this._super(...arguments);
-    this.set('page', 1);
-    this.set('perPage', 20);
-    this.set('things', this._generateThings());
-  },
+    super.init(...arguments);
+    set(this, 'page', 1);
+    set(this, 'perPage', 20);
+    set(this, 'things', this._generateThings());
+  }
 
-  appController: injectController('application'),
-
-  actions: {
-    loadMore() {
-      return new Promise(resolve => {
-        later(() => {
-          this.incrementProperty('page');
-          this.get('things').pushObjects(this._generateThings());
-          resolve();
-        }, this.get('appController.loadDelay'));
-      });
-    }
-  },
+  @action
+  loadMore() {
+    return new Promise(resolve => {
+      later(() => {
+        this.incrementProperty('page');
+        this.things.pushObjects(this._generateThings());
+        resolve();
+      }, get(this, 'appController.loadDelay'));
+    });
+  }
 
   _generateThings() {
-    const start = this.getWithDefault('things.length', 0) + 1;
-    const end = this.get('page') * this.get('perPage');
+    const start = getWithDefault(this, 'things.length', 0) + 1;
+    const end = this.page * this.perPage;
     return generateThings(start, end);
   }
-});
+}
