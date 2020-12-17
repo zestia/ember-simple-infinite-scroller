e01b7bde3adcbaef459ffb521abb566d17c2c716
Andrew Kirwin <andrew.kirwin@gmail.com>
2020-12-17 21:34:19 +0000

    continue refactor

--- tests/dummy/app/controllers/example-2.js
+++ tests/dummy/app/controllers/example-2.js
@@ -1,7 +1,5 @@
 import ThingsController from '../_controllers/things';
 
 export default class Example2Controller extends ThingsController {
-  get documentElement() {
-    return document;
-  }
+  document = document;
 }
