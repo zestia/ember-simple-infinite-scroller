e4b069a0d264596830624c8454c63ecabcf507b2
Andrew Kirwin <github@andrewkirwin.co.uk>
2023-01-27 00:08:04 +0000

    Run ember-cli-update

--- tests/dummy/config/environment.js
+++ tests/dummy/config/environment.js
@@ -7,11 +7,11 @@ module.exports = function (environment) {
     rootURL: '/',
     locationType: 'history',
     EmberENV: {
+      EXTEND_PROTOTYPES: false,
       FEATURES: {
         // Here you can enable experimental features on an ember canary build
-        // e.g. 'with-controller': true
-      },
-      EXTEND_PROTOTYPES: false
+        // e.g. EMBER_NATIVE_DECORATOR_SUPPORT: true
+      }
     },
 
     APP: {
@@ -41,7 +41,6 @@ module.exports = function (environment) {
   }
 
   if (environment === 'production') {
-    ENV.locationType = 'hash';
     ENV.rootURL = '/ember-simple-infinite-scroller/';
   }
 
